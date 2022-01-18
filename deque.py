523307beb20d3a18b7b833c4d3747c45b6e33c3d
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-01-18 23:47:55 +0300

    Deque

---
+++ deque.py
@@ -0,0 +1,68 @@
+class Deque:
+    def __init__(self, n):
+        self._max_size = n
+        self._buffer = [None] * n
+        self._size = 0
+        self._head = 0
+        self._tail = 0
+
+    def push_back(self, value):
+        if self._size == self._max_size:
+            return 'error'
+        self._buffer[self._tail] = value
+        self._tail = (self._tail + 1) % self._max_size
+        self._size += 1
+        return None
+
+    def push_front(self, value):
+        if self._size == self._max_size:
+            return 'error'
+        # я знаю про обратную индексацию в питоне, но так алгоритмически правильнее
+        self._head = (self._head - 1 + self._max_size) % self._max_size
+        self._buffer[self._head] = value
+        self._size += 1
+        return None
+
+    def pop_back(self):
+        if self._size == 0:
+            return None, 'error'
+        self._tail = (self._tail - 1 + self._max_size) % self._max_size
+        value = self._buffer[self._tail]
+        self._buffer[self._tail] = None
+        self._size -= 1
+        return value, None
+
+    def pop_front(self):
+        if self._size == 0:
+            return None, 'error'
+        value = self._buffer[self._head]
+        self._buffer[self._head] = None
+        self._head = (self._head + 1) % self._max_size
+        self._size -= 1
+        return value, None
+
+
+def main():
+    n = int(input())
+    m = int(input())
+    deque = Deque(m)
+    for _ in range(n):
+        command = input().split()
+        if command[0] == 'push_back':
+            err = deque.push_back(command[1])
+            if err is not None:
+                print(err)
+        elif command[0] == 'push_front':
+            err = deque.push_front(command[1])
+            if err is not None:
+                print(err)
+        elif command[0] == 'pop_front':
+            res, err = deque.pop_front()
+            print(res if res is not None else err)
+        elif command[0] == 'pop_back':
+            res, err = deque.pop_back()
+            print(res if res is not None else err)
+
+
+if __name__ == '__main__':
+    main()
