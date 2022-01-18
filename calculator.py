d48d442dd5c7fdd52a4288cde58103a9baf210c0
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-01-18 22:42:59 +0300

    Calculator

---
+++ calculator.py
@@ -0,0 +1,61 @@
+import sys
+
+
+class Stack:
+    def __init__(self):
+        self._head = None,
+        self._size = 0
+
+    def __str__(self):
+        cur = self._head
+        result = ""
+        while cur:
+            result += str(cur.value) + ' -> '
+            cur = cur.prev
+        return result[:-5]
+
+    class Node:
+        def __init__(self, value, prev=None):
+            self.value = value
+            self.prev = prev
+
+    def push(self, value):
+        node = self.Node(value, self._head)
+        self._head = node
+        self._size += 1
+
+    def pop(self):
+        if self._size == 0:
+            raise ValueError('Stack is empty')
+        node = self._head
+        self._head = self._head.prev
+        self._size -= 1
+        return node.value
+
+    def size(self):
+        return self._size
+
+
+def main():
+    stack = Stack()
+    line = sys.stdin.readline().rstrip().split()
+    for ch in line:
+        if ch not in ('+', '-', '*', '/'):
+            stack.push(int(ch))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if ch == '+':
+                res = a + b
+            elif ch == '-':
+                res = a - b
+            elif ch == '*':
+                res = a * b
+            elif ch == '/':
+                res = a // b
+            stack.push(res)
+    print(stack.pop())
+
+
+if __name__ == '__main__':
+    main()
