087ee7c089e5f084f96ebb7ca04eede941a67b0d
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-01-20 01:22:25 +0300

    Fixes after review

--- src/s2/calculator.py
+++ src/s2/calculator.py
@@ -8,44 +8,17 @@
 # либо достаю их оттуда (сразу 2) тоже за O(1)
 # Доказательство: если предполагается n арифметических операций, то в среднем/худшем случае это будет:
 # (2 вставки + 2 извлечения + 1 вставка (результат)) * n операций, т.е. O(n).
-# Отчет 64072427 https://contest.yandex.ru/contest/22781/run-report/64072427/
+# Отчет 64130598 https://contest.yandex.ru/contest/22781/run-report/64130598/
 
 import sys
 
 
-class Stack:
-    def __init__(self):
-        self._head = None,
-        self._size = 0
-
-    class Node:
-        def __init__(self, value, prev=None):
-            self.value = value
-            self.prev = prev
-
-    def push(self, value):
-        node = self.Node(value, self._head)
-        self._head = node
-        self._size += 1
-
-    def pop(self):
-        if self._size == 0:
-            raise ValueError('Stack is empty')
-        node = self._head
-        self._head = self._head.prev
-        self._size -= 1
-        return node.value
-
-    def size(self):
-        return self._size
-
-
 def main():
-    stack = Stack()
+    stack = []
     line = sys.stdin.readline().rstrip().split()
     for ch in line:
         if ch not in ('+', '-', '*', '/'):
-            stack.push(int(ch))
+            stack.append(int(ch))
         else:
             b = stack.pop()
             a = stack.pop()
@@ -57,7 +30,7 @@ def main():
                 res = a * b
             elif ch == '/':
                 res = a // b
-            stack.push(res)
+            stack.append(res)
     print(stack.pop())
 
 
--- src/s2/deque.py
+++ src/s2/deque.py
@@ -5,7 +5,7 @@
 # Поскольку буффер кольцевой, все операции над указателями происходят по модулю длиный массива.
 # Дека потребялет константый размер памяти O(n)
 # Ошибки передаю в качестве возвращаемого значения (как объект или вторым параметром кортежа).
-# Отчет 64074207 https://contest.yandex.ru/contest/22781/run-report/64074207/
+# Отчет 64130697 https://contest.yandex.ru/contest/22781/run-report/64130697/
 
 
 class Deque:
@@ -18,38 +18,36 @@ class Deque:
 
     def push_back(self, value):
         if self._size == self._max_size:
-            return 'error'
+            raise ValueError
         self._buffer[self._tail] = value
         self._tail = (self._tail + 1) % self._max_size
         self._size += 1
-        return None
 
     def push_front(self, value):
         if self._size == self._max_size:
-            return 'error'
+            raise ValueError
         # я знаю про обратную индексацию в питоне, но так алгоритмически правильнее
         self._head = (self._head - 1 + self._max_size) % self._max_size
         self._buffer[self._head] = value
         self._size += 1
-        return None
 
     def pop_back(self):
         if self._size == 0:
-            return None, 'error'
+            raise ValueError
         self._tail = (self._tail - 1 + self._max_size) % self._max_size
         value = self._buffer[self._tail]
         self._buffer[self._tail] = None
         self._size -= 1
-        return value, None
+        return value
 
     def pop_front(self):
         if self._size == 0:
-            return None, 'error'
+            raise ValueError
         value = self._buffer[self._head]
         self._buffer[self._head] = None
         self._head = (self._head + 1) % self._max_size
         self._size -= 1
-        return value, None
+        return value
 
 
 def main():
@@ -58,20 +56,12 @@ def main():
     deque = Deque(m)
     for _ in range(n):
         command = input().split()
-        if command[0] == 'push_back':
-            err = deque.push_back(command[1])
-            if err is not None:
-                print(err)
-        elif command[0] == 'push_front':
-            err = deque.push_front(command[1])
-            if err is not None:
-                print(err)
-        elif command[0] == 'pop_front':
-            res, err = deque.pop_front()
-            print(res if res is not None else err)
-        elif command[0] == 'pop_back':
-            res, err = deque.pop_back()
-            print(res if res is not None else err)
+        try:
+            res = getattr(deque, command[0])(*command[1:])
+            if res is not None:
+                print(res)
+        except ValueError:
+            print('error')
 
 
 if __name__ == '__main__':
