f247ce6c69e1e393482bbdf508b47e3ed4a5a78b
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-02-13 01:08:32 +0300

    Fix shaker sort

--- src/s3/cocktail_shaker_sort.py
+++ src/s3/cocktail_shaker_sort.py
@@ -7,22 +7,18 @@ def main():
 def cocktail_shaker_sort(array):
     left = 0
     right = len(array) - 1
-    swapped = True
-    while swapped:
-        swapped = False
+    control = right
+    while left < right:
         for i in range(left, right, 1):
             if array[i] > array[i + 1]:
                 array[i], array[i + 1] = array[i + 1], array[i]
-                swapped = True
-        right -= 1
-        if not swapped:
-            break
-        swapped = False
+                control = i
+        right = control
         for i in range(right, left, -1):
             if array[i] < array[i - 1]:
                 array[i], array[i - 1] = array[i - 1], array[i]
-                swapped = True
-        left += 1
+                control = i
+        left = control
 
 
 if __name__ == '__main__':
