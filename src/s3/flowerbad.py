e5839da67a4284e1e24de6b73b38203398ac9c5b
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-03-10 22:55:36 +0400

    Flowerbed

---
+++ src/s3/flowerbad.py
@@ -0,0 +1,24 @@
+def flowerbed(n, array):
+    array.sort()
+    result = []
+    (left, right) = array[0]
+    for i in range(1, n):
+        (next_left, next_right) = array[i]
+        if right >= next_left:
+            right = max(right, next_right)
+        else:
+            result.append((left, right))
+            left, right = next_left, next_right
+    result.append((left, right))
+    return result
+
+
+if __name__ == '__main__':
+    n = int(input())
+    pieces = [None] * n
+    for i in range(n):
+        parts = input().rstrip().split()
+        pieces[i] = (int(parts[0]), int(parts[1]))
+    result = flowerbed(n, pieces)
+    for tuple in result:
+        print(tuple[0], tuple[1])
