cbddad40805132c05846c9b5541eff1a10d17083
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-05-23 23:35:05 +0300

    Uncomment

--- src/s5/search_tree.py
+++ src/s5/search_tree.py
@@ -1,9 +1,9 @@
-# # Comment it before submitting
-# class Node:
-#     def __init__(self, value, left=None, right=None):
-#         self.value = value
-#         self.right = right
-#         self.left = left
+# Comment it before submitting
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.right = right
+        self.left = left
 
 
 def solution(root) -> bool:
