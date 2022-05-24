8746804ea776b62eef23c7d2ecd148e621db786e
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-05-25 00:25:00 +0300

    Implement insert into BST

---
+++ src/s5/add_node.py
@@ -0,0 +1,34 @@
+# Uncomment it before submitting
+from node import Node
+
+
+# Comment it before submitting
+# class Node:
+#     def __init__(self, left=None, right=None, value=0):
+#         self.right = right
+#         self.left = left
+#         self.value = value
+
+
+# Return new head of the tree
+def insert(root, key) -> Node:
+    if root is None:
+        return Node(None, None, key)
+    if key < root.value:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
+
+
+def test():
+    node1 = Node(None, None, 7)
+    node2 = Node(node1, None, 8)
+    node3 = Node(None, node2, 7)
+    new_head = insert(node3, 6)
+    assert new_head is node3
+    assert new_head.left.value == 6
+
+
+if __name__ == '__main__':
+    test()
