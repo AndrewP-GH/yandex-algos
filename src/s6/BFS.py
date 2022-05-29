ecebfbb590d07ab6738174914a9208066f675cb9
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-05-29 14:40:33 +0300

    Upgrade BFS

--- src/s6/BFS.py
+++ src/s6/BFS.py
@@ -1,26 +1,8 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
-def bfs(edges, n, s):
-    queue = [None] * n
-    count = 0
-    color = [False] * (n + 1)
-    planned = []
-    color[s] = True
-    planned.append(s)
-    while len(planned) > 0:
-        v = planned.pop(0)
-        queue[count] = v
-        count += 1
-        for u in edges[v]:
-            if not color[u]:
-                color[u] = True
-                planned.append(u)
-    print(" ".join(map(str, queue[:count])))
-
-
-if __name__ == '__main__':
+def init():
     line = sys.stdin.readline().split()
     n = int(line[0])
     m = int(line[1])
@@ -32,6 +14,28 @@ if __name__ == '__main__':
         edges[u].append(v)
         edges[v].append(u)
     for k in edges.keys():
-        edges[k].sort()
+        edges[k] = sorted(edges[k])
     s = int(sys.stdin.readline())
+    return edges, n, s
+
+
+def bfs(edges, n, s):
+    queue = [None] * n
+    count = 0
+    color = [False] * (n + 1)
+    color[s] = True
+    planned = deque([s])
+    while len(planned) > 0:
+        v = planned.popleft()
+        queue[count] = v
+        count += 1
+        for u in edges[v]:
+            if not color[u]:
+                color[u] = True
+                planned.append(u)
+    print(" ".join(map(str, queue[:count])))
+
+
+if __name__ == '__main__':
+    edges, n, s = init()
     bfs(edges, n, s)
