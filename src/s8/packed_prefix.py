8f3543cf2aa060c9d11872b89fbbc746474d007a
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-06-23 18:06:17 +0300

    Done

--- src/s8/packed_prefix.py
+++ src/s8/packed_prefix.py
@@ -3,33 +3,41 @@ import unittest
 
 
 def packed_prefix(n, lines):
-    pass
+    result = ''
+    for i in range(len(lines[0])):
+        ch = lines[0][i]
+        for j in range(1, n):
+            line = lines[j]
+            if i > len(line) or line[i] != ch:
+                return result
+        result += ch
+    return result
 
 
-def build_line(line):
+def unpack(line):
     stack = []
     i = 0
-    local = ''
+    brackets = 0
     while i < len(line):
         ch = line[i]
-        if 'a' <= ch <= 'z':
-            local += ch
-        elif len(local) > 0:
-            stack.append(local)
+        if ch == '[':
+            brackets += 1
+        elif ch == ']':
+            brackets -= 1
+            top = stack.pop()
             local = ''
-        elif ch.isnumeric():
-            stack.append(int(ch))
+            while not top.isnumeric():
+                local = top + local
+                top = stack.pop()
+            local = int(top) * local
+            stack.append(local)
+        else:
+            stack.append(ch)
         i += 1
-    if len(local) > 0:
-        stack.append(local)
 
     result = ''
     while len(stack) > 0:
-        local = stack.pop()
-        if isinstance(local, int):
-            result = result * local
-        else:
-            result = local + result
+        result = stack.pop() + result
     return result
 
 
@@ -37,37 +45,50 @@ if __name__ == '__main__':
     _n = int(input())
     _lines = [None] * _n
     for _i in range(_n):
-        _lines[_i] = sys.stdin.readline().rstrip()
-    packed_prefix(_n, _lines)
+        _line = sys.stdin.readline().rstrip()
+        _lines[_i] = unpack(_line)
+    print(packed_prefix(_n, _lines))
 
 
 class TestPackedPrefix(unittest.TestCase):
     def test_build_line_1(self):
         line = '2[a]2[ab]'
         exp = 'aaabab'
-        self.assertEqual(build_line(line), exp)
+        self.assertEqual(unpack(line), exp)
 
     def test_build_line_2(self):
         line = '3[a]2[r2[t]]'
         exp = 'aaarttrtt'
-        self.assertEqual(build_line(line), exp)
+        self.assertEqual(unpack(line), exp)
 
     def test_build_line_3(self):
         line = 'a2[aa3[b]]'
         exp = 'aaabbbaabbb'
-        self.assertEqual(build_line(line), exp)
+        self.assertEqual(unpack(line), exp)
 
     def test_build_line_4(self):
         line = '2[abac]a'
         exp = 'abacabaca'
-        self.assertEqual(build_line(line), exp)
+        self.assertEqual(unpack(line), exp)
 
     def test_build_line_5(self):
         line = '3[aba]'
         exp = 'abaabaaba'
-        self.assertEqual(build_line(line), exp)
+        self.assertEqual(unpack(line), exp)
 
     def test_build_line_6(self):
         line = '3[aba]c'
         exp = 'abaabaabac'
-        self.assertEqual(build_line(line), exp)
+        self.assertEqual(unpack(line), exp)
+
+    def test_max_prefix_1(self):
+        n = 3
+        lines = ['aaabab', 'aaarttrtt', 'aaabbbaabbb']
+        exp = 'aaa'
+        self.assertEqual(packed_prefix(n, lines), exp)
+
+    def test_max_prefix_2(self):
+        n = 3
+        lines = ['abacabaca', 'abacabaca', 'abaabaaba']
+        exp = 'aba'
+        self.assertEqual(packed_prefix(n, lines), exp)
