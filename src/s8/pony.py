a851ab5e3f0b30eb91d9310e57d3872e36ac829c
Andrey Paramonov <a.paramonov@dodopizza.com>
2022-06-25 15:17:07 +0300

    TL again

--- src/s8/pony.py
+++ src/s8/pony.py
@@ -1,22 +1,21 @@
 import sys
 import unittest
 
-end_word_marker = '$'
-
 
 def is_pony(string: str, words: [str]) -> bool:
-    tree = build_prefix_tree(words)
+    end_word_marker = '$'
+    tree = build_prefix_tree(words, end_word_marker)
     possible_ways = set()
     possible_ways.add(0)
     while len(possible_ways) > 0:
         pos = next(iter(possible_ways))
         possible_ways.remove(pos)
-        if check_string(string[pos:], tree, pos, possible_ways):
+        if check_string(string[pos:], tree, end_word_marker, pos, possible_ways):
             return True
     return False
 
 
-def check_string(string: str, tree: dict, shift: int, possible_ways: set) -> bool:
+def check_string(string: str, tree: dict, end_word_marker: str, shift: int, possible_ways: set) -> bool:
     node = tree
     i = 0
     len_s = len(string)
@@ -35,11 +34,13 @@ def check_string(string: str, tree: dict, shift: int, possible_ways: set) -> boo
     return i == len_s and end_word_marker in node
 
 
-def build_prefix_tree(words: [str]) -> dict:
+def build_prefix_tree(words: [str], end_word_marker: str) -> dict:
     tree = {}
     for word in words:
         node = tree
         for letter in word:
+            if letter == '\n':
+                continue
             if letter not in node:
                 node[letter] = {}
             node = node[letter]
@@ -52,7 +53,7 @@ if __name__ == '__main__':
     _n = int(input())
     _words = [None] * _n
     for _i in range(_n):
-        _words[_i] = sys.stdin.readline().rstrip()
+        _words[_i] = sys.stdin.readline()
     _result = is_pony(_t, _words)
     print('YES' if _result else 'NO')
 
